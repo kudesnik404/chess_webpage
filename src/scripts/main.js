3ac31e9f131fe3ed640731b3008d463cf46274c0
Victoria Demidenko <demidenko@adsfin.pro>
2026-05-17 00:18:24 +0300

    second, third sections and footer

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -12,6 +12,26 @@ const texts = [
     'Лед тронулся, господа присяжные заседатели!',
 ];
 
-const container = document.getElementById('marquee');
+document.querySelectorAll('.marquee-id').forEach(container => {
+    container.appendChild(createMarquee(texts));
+});
 
-container.appendChild(createMarquee(texts));
+// text reveal
+document.addEventListener('DOMContentLoaded', () => {
+    const elements = document.querySelectorAll('.text-reveal');
+
+    const observer = new IntersectionObserver((entries, obs) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('is-visible');
+
+                // animate only once
+                obs.unobserve(entry.target);
+            }
+        });
+    }, {
+        threshold: 0.2
+    });
+
+    elements.forEach(el => observer.observe(el));
+});forEach(el => observer.observe(el));
