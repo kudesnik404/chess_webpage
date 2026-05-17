da6a4cde5a869177a8319592b04ebea1dca31d95
Victoria Demidenko <demidenko@adsfin.pro>
2026-05-18 00:20:27 +0300

    participants section (no js)

---
+++ src/scripts/participants.js
@@ -0,0 +1,36 @@
+const participants= [
+    {
+        name: "Хозе-Рауль Капабланка",
+        title: "Чемпион мира по шахматам",
+        photo: "../../images/participants__item.png",
+        link: "#",
+    },
+    {
+        name: "Эммануил Ласкер",
+        title: "Чемпион мира по шахматам",
+        photo: "",
+        link: "#",
+    },
+    {
+        name: "Александр Алехин",
+        title: "Чемпион мира по шахматам",
+        link: "#",
+    },
+    {
+        name: "Арон Нимцович",
+        title: "Чемпион мира по шахматам",
+        photo: "../../images/participants__item.png",
+        link: "#",
+    },
+    {
+        name: "Рихард Рети",
+        title: "Чемпион мира по шахматам",
+        link: "#",
+    },
+    {
+        name: "Остап Бендер",
+        photo: "../../images/participants__item.png",
+        title: "Гроссмейстер",
+        link: "#",
+    },
+]
